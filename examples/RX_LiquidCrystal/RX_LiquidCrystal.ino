951bd740813049d8ef650e76ef3edf1d070ca2d3
victorboria <kelsodesigns@gmail.com>
2015-03-27 21:32:50 +0100

    Examples for LCD
    Examples for I2C Liquid Crystal 16x2 LCD’s

---
+++ examples/RX_LiquidCrystal/RX_LiquidCrystal.ino
@@ -0,0 +1,98 @@
+#include <XBee_AT.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+
+
+//CAMBIA ESTOS VALORES POR LOS TUYOS:
+uint16_t red = 0x3332;    //ID de la red
+char canal = 'C';         //Canal de la red
+char baudrate = '3';      //9600
+
+char response_a[20];
+char response_b[20];
+
+
+void setup() {    
+  Serial.begin(9600);
+  
+  lcd.begin(16, 2);
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("Bienvenido a");
+  lcd.setCursor(0, 1);
+  lcd.print("Arduino Day Zgz");
+  delay(2000);
+  
+  XBee.entraConfig();
+  delay(500);
+   
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Configurando    ");
+  lcd.setCursor(0, 1);
+  lcd.print("XBee           ");
+
+  XBee.restauraConfig();
+  delay(500);
+
+  XBee.cambiaID(red); 
+  delay(500);
+  
+  XBee.cambiaCanal(canal); 
+  delay(500);
+  
+  XBee.cambioBD(baudrate);
+  delay(500);
+  
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Saliendo de     ");
+  lcd.setCursor(0, 1);
+  lcd.print("de configuracion");
+  
+  XBee.guardaConfig();
+  delay(500);
+ 
+  XBee.salirConfig(); 
+  delay(500);
+  
+  
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Esperando      ");
+  lcd.setCursor(0, 1);
+  lcd.print("mensajes...    ");
+  
+  while(Serial.available()>0){Serial.read();}
+}
+
+void loop() {
+  if(Serial.available() > 32){
+    for(int i;i<16;i++){
+      response_a[i] = Serial.read();
+    }
+    for(int i;i<16;i++){
+      response_b[i] = Serial.read();
+    }
+    while (Serial.available() > 0)
+    {
+      Serial.read();
+    }
+  }
+  
+  lcd.setCursor(0, 0);
+  lcd.print(response_a);
+  lcd.setCursor(0, 1);
+  lcd.print(response_b);
+}
+
+
+
+
+
+
+
+
+
+
---
+++ examples/TX_LiquidCrystal/TX_LiquidCrystal.ino
@@ -0,0 +1,97 @@
+#include <XBee_AT.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+
+
+//CAMBIA ESTOS VALORES POR LOS TUYOS:
+uint16_t red = 0x3332;    //ID de la red
+char canal = 'C';         //Canal de la red
+char baudrate = '3';      //9600
+uint32_t DH = 0x13A200;   //Parte alta de la MAC del destinatario
+uint32_t DL = 0x408BCE55; //Parte baja de la MAC del destinatario
+char response_a1[17] = "Pantalla 1:     "; //Escribe 16 caracteres
+char response_b1[17] = "Cambia el  texto"; //Escribe 16 caracteres
+char response_a2[17] = "Pantalla 2:     "; //Escribe 16 caracteres
+char response_b2[17] = "Segundo texto   "; //Escribe 16 caracteres
+
+
+void setup() {    
+  Serial.begin(9600);
+
+  lcd.begin(16, 2);
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("Bienvenido a");
+  lcd.setCursor(0, 1);
+  lcd.print("Arduino Day Zgz");  
+  delay(2000);
+  
+  XBee.entraConfig();
+  delay(500);
+   
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Configurando    ");
+  lcd.setCursor(0, 1);
+  lcd.print("XBee           ");
+
+  XBee.restauraConfig();
+  delay(500);
+  
+  //Mensaje que recibiran todos en mi misma ID de red
+  //XBee.modoBroadcast(); 
+  
+  //Mensaje que solo recibira el XBee con esta MAC
+  XBee. modoUnicast(DH, DL);
+  delay(500);
+
+  XBee.cambiaID(red); 
+  delay(500);
+  
+  XBee.cambiaCanal(canal); 
+  delay(500);
+  
+  XBee.cambioBD(baudrate);
+  delay(500);
+  
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Saliendo de     ");
+  lcd.setCursor(0, 1);
+  lcd.print("de configuracion");
+  
+  XBee.guardaConfig();
+  delay(500);
+ 
+  XBee.salirConfig(); 
+  delay(1000);
+}
+
+void loop() {
+  
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(response_a1);
+  lcd.setCursor(0, 1);
+  lcd.print(response_b1);
+  
+  Serial.print(response_a1);
+  Serial.println(response_b1);
+  delay(4000);
+  
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(response_a2);
+  lcd.setCursor(0, 1);
+  lcd.print(response_b2);
+  
+  Serial.print(response_a2);
+  Serial.println(response_b2);
+  delay(4000);
+}
+
+
+
+
+
